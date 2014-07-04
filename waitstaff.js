3aa787ef10ccba554ef07a4b2994864591a9154b
petejh <petejh@truly.fake>
2014-07-04 16:57:56 -0700

    Break calculator panels into view partials and define internal routing
    A side effect is that the nifty scope event messaging in the original
    solution had to be replaced by a mechanism that would allow state to
    persist across routes. Still waiting to learn how to write custom
    services, so the important variables are registered in a value service.

--- waitstaff.js
+++ waitstaff.js
@@ -1,78 +1,113 @@
-angular.module("WaitstaffCalculator", [])
+angular.module('WaitstaffCalculator', ['ngRoute'])
 
-  .controller("CalculatorCtrl", function($scope) {
-    $scope.resetCalculator = function() {
-      $scope.$broadcast('resetCalculator');
-    };
+  .value('earnings', { tipTotal: 0, mealCount: 0 })
+
+  .config(function($routeProvider) {
+    $routeProvider.when('/', {
+      templateUrl: 'home.html'
+    }).when('/new-meal', {
+      templateUrl: 'new-meal.html',
+      controller: 'MealTicketCtrl'
+    }).when('/my-earnings', {
+      templateUrl: 'my-earnings.html',
+      controller: 'EarningsCtrl'
+    }).otherwise({
+      redirectTo: '/'
+    });
   })
 
-  .controller("MealDetailsCtrl", function($scope, $rootScope) {
-    var data = {
+  .controller("CalculatorCtrl", function($scope) {
+    // nothing, yet, to do
+  })
+
+  .controller("MealTicketCtrl", function($scope, $rootScope, earnings) {
+    var data = $scope.data = {
       basePrice: 0,
       taxRate: 0,
       tipPercent: 0
     };
-    function resetData() {
-      for (item in data) {
-        data[item] = 0;
-      }
-      $scope.data = data;
-    }
-    $scope.submit = function() {
-      $rootScope.$broadcast('ticketPaid', $scope.data);
-    };
-    $scope.cancel = function() {
-      resetData();
-    };
-    $scope.$on('resetCalculator', function() {
-      resetData();
-    });
-    /* Initializations */
-    $scope.data = data;
-  })
 
-  .controller("CustomerChargesCtrl", function($scope, $rootScope) {
-    var data = {
+    var charges = $scope.charges = {
       subtotal: 0,
       tip: 0
     };
+
     function resetData() {
       for (item in data) {
         data[item] = 0;
       }
-      $scope.data = data;
     }
-    $scope.$on('ticketPaid', function(event, ticketData) {
-      $scope.data.subtotal = ticketData.basePrice * (1 + ticketData.taxRate / 100);
-      $scope.data.tip = $scope.data.subtotal * (ticketData.tipPercent / 100);
-      $rootScope.$broadcast('tipReceived', $scope.data.tip);
-    });
-    $scope.$on('resetCalculator', function() {
+
+    function clearCharges() {
+      for (item in charges) {
+        charges[item] = 0;
+      }
+    }
+
+    function calculateCharges() {
+      charges.subtotal = data.basePrice * (1 + data.taxRate / 100);
+      charges.tip = charges.subtotal * (data.tipPercent / 100);
+    }
+
+    function calculateEarnings() {
+      earnings.tipTotal += charges.tip;
+      earnings.mealCount++;
+    }
+
+    $scope.validCurrency = function(value) {
+      return (/^(\d+(\.\d?\d?)?|\.\d\d?)$/.test(value));
+    };
+
+    $scope.validPercentage = function(value) {
+      return (/^(100(\.0*)?|[1-9]?\d(\.\d*)?|\.\d+)$/.test(value));
+    };
+
+    function validForm() {
+      return ($scope.validCurrency(data.basePrice)
+        && $scope.validPercentage(data.taxRate)
+        && $scope.validPercentage(data.tipPercent)
+      );
+    }
+
+    $scope.submit = function() {
+      if (validForm()) {
+        calculateCharges();
+        // $rootScope.$broadcast('tipReceived', charges.tip);
+        calculateEarnings();
+      }
+    };
+
+    $scope.cancel = function() {
       resetData();
-    });
-    /* Initializations */
-    $scope.data = data;
+      clearCharges();
+    };
   })
 
-  .controller("EarningsInfoCtrl", function($scope) {
-    var data = {
+  .controller("EarningsCtrl", function($scope, earnings) {
+    /*
+    var data = $scope.data = {
       tipTotal: 0,
       mealCount: 0
     };
+    */
+
+    var data = $scope.data = earnings;
+
     function resetData() {
       for (item in data) {
         data[item] = 0;
       }
-      $scope.data = data;
     }
+
+    /*
     $scope.$on('tipReceived', function(event, tip) {
-      $scope.data.tipTotal += tip;
-      $scope.data.mealCount++;
+      data.tipTotal += tip;
+      data.mealCount++;
     });
-    $scope.$on('resetCalculator', function() {
+    */
+
+    $scope.resetCalculator = function() {
       resetData();
-    });
-    /* Initializations */
-    $scope.data = data;
+    };
   });
 
